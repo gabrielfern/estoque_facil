c9fed03566bdb6a74a5dbf200d2651c764af4d66
gabrielfernandesufcg <gabriel.fernandes.silva@ccc.ufcg.edu.br>
2018-02-23 16:25:22 -0300

    tirado comentario da classe EstoqueFacilRestApi

--- src/main/java/com/ufcg/si1/EstoqueFacilRestApi.java
+++ src/main/java/com/ufcg/si1/EstoqueFacilRestApi.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-@SpringBootApplication(scanBasePackages={"com.ufcg.si1"})// same as @Configuration @EnableAutoConfiguration @ComponentScan combined
+@SpringBootApplication(scanBasePackages={"com.ufcg.si1"})
 public class EstoqueFacilRestApi {
 
 	public static void main(String[] args) {
