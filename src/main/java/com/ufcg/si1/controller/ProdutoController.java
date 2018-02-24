2bb87dbe2d783ff843296142b83cb370da31aee1
Yuri Kelvin <yuri.silva@ccc.ufcg.edu.br>
2018-02-24 19:04:05 -0300

    Removido imports desnecessários de ProdutoController

--- src/main/java/com/ufcg/si1/controller/ProdutoController.java
+++ src/main/java/com/ufcg/si1/controller/ProdutoController.java
@@ -6,14 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ufcg.si1.model.Lote;
 import com.ufcg.si1.model.Produto;
-import com.ufcg.si1.model.enums.Situacao;
 import com.ufcg.si1.service.ProdutoService;
 
 
