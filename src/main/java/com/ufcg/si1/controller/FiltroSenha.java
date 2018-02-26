49b4dec0f25f9deb621fd1dfa0d21efea8844935
Yuri Kelvin <yuri.silva@ccc.ufcg.edu.br>
2018-02-26 19:05:36 -0300

    Adicionado HTTP Status Unauthorized quando ha ausencia de senha na url

--- src/main/java/com/ufcg/si1/controller/FiltroSenha.java
+++ src/main/java/com/ufcg/si1/controller/FiltroSenha.java
@@ -1,5 +1,6 @@
 package com.ufcg.si1.controller;
 
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.filter.GenericFilterBean;
 
 import javax.servlet.FilterChain;
@@ -17,17 +18,22 @@ public class FiltroSenha extends GenericFilterBean {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 
+
         String senhaAdmin = "banana";
 
         HttpServletRequest req = (HttpServletRequest) request;
 
+
+
         String senha = req.getParameter("senha");
 
-        if(!senhaAdmin.equals(senha)) {
-            ((HttpServletResponse) response).sendError(HttpServletResponse.SC_UNAUTHORIZED);
-        }
 
-        chain.doFilter(request, response);
+
+        if(senha == null || !senhaAdmin.equals(senha)) 
+            ((HttpServletResponse) response).sendError(HttpServletResponse.SC_UNAUTHORIZED);
+         else
+            chain.doFilter(request, response);
+
     }
 
 
