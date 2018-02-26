b27af7b8028a863c3d0c932a5d8ed1311a3e24c0
Gabriel Fernandes <gabrielfernndss@gmail.com>
2018-02-26 14:40:11 -0300

    removido LoteController

--- src/main/java/com/ufcg/si1/controller/AdminController.java
+++ src/main/java/com/ufcg/si1/controller/AdminController.java
@@ -1,7 +1,5 @@
 package com.ufcg.si1.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ufcg.si1.model.ItemVenda;
 import com.ufcg.si1.model.Lote;
 import com.ufcg.si1.model.Produto;
 import com.ufcg.si1.model.Venda;
